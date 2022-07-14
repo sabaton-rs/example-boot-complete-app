13bd69e2a9d7116a4728db63d403a5f1247158c1
anaghar8921 <anagha.r@acsiatech.com>
2022-07-14 17:55:04 +0530

    Added code to verify the working of application

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use libcore::bootloader::bootcontrol::BootControlImpl;
 
 pub fn set_successful_boot()->Result<()> {
     let mut bootctrl = BootControlImpl::create()?;
+    let active_slot=bootctrl.active_slot()?;
+    print!("Current active slot is {}",active_slot);
     bootctrl.set_boot_successful()?;
     Ok(())
 }
